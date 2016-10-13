6e3a45f13ad6700d90b73d7e1048f81b5e8eb066
Erwin Rozada <erwin.rozada@gmail.com>
2016-10-13 06:19:34 -0400

    Began work on NHibernate mapping

---
+++ SimpleBlog/Database.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NHibernate.Cfg;
+
+namespace SimpleBlog
+{
+    public static class Database
+    {
+        public static void Configure()
+        {
+            var config = new Configuration();
+
+            // configure the connection string
+            config.Configure();
+
+            // add our mappings
+
+            // create session factory
+        }
+
+        public static void OpenSession()
+        {
+
+        }
+
+        public static void CloseSession()
+        {
+
+        }
+    }
+}
